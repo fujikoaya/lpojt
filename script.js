fbe9ee1b8e503e2a12c190b3aacebb50c8a68a51
443YOSHI\yosin <yosin330@yahoo.co.jp>
2022-10-18 13:59:17 +0900

    フェードインが動かない

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
 $(function(){
+    // Petlivers
+	$(window).on('load scroll',function (){
+		$('.petlivers').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+    // About
+    $(window).on('load scroll',function (){
+		$('.about').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+    // Sponser
+    $(window).on('load scroll',function (){
+		$('.sponser').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
+
+$(function(){
     // service
 	$(window).on('load scroll',function (){
 		$('.customer').each(function(){
